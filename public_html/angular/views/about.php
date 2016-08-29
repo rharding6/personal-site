5319072574706957f5a12dbd345dece505ccfee5
Robert Harding <robisonfire@gmail.com>
2016-08-29 11:34:35 -0600

    changed one <span> to a <p>

--- public_html/angular/views/about.php
+++ public_html/angular/views/about.php
@@ -12,7 +12,7 @@
 		<p class="resumeSummation">After graduating from the University of New Mexico, our founder and (current) Hed
 			Shredder learned basic front-end code techniques on various instructional websites such as codecademy.com and
 			formalized computing and development skills at the CNM STEMULUS Ingenuity Center Deep Dive Coding Bootcamp.
-			Always looking for new challenges, he remain sopen to freelance or contract web-development work and assists
+			Always looking for new challenges, he remains open to freelance or contract web-development work and assists
 			his colleagues to his best ability. In addition to seeking full-time employment, Robert plans on returning to
 			UNM to pursue an M.S. in Computer Science.</p>
 	</div>
@@ -22,7 +22,7 @@
 		<a href="http://ling.unm.edu/"><img class="logo" src="img/unm-logo.png"/></a>
 		<a href="http://stemuluscenter.org/"><img class="logo" src="img/stemulus-center-logo.png"/></a>
 		<a href="https://www.codecademy.com/"><img class="logo" src="img/codecademy-logo.png"/></a>
-		<span>our founder received most of his formal training through these institutions</span>
+		<p>our founder received most of his formal training through these institutions</p>
 	</div>
 
 </div>
